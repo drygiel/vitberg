e68f8f4e1a81c1383d1008367c99751e52bc3b0a
Damian Drygiel <damian.drygiel@gmail.com>
2026-06-11 02:26:47 +0200

    correct repository name casing in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-const repoName = 'Vitberg';
+const repoName = 'vitberg';
 
 const nextConfig: NextConfig = {
   output: 'export',
